2ccb353ac77e372e1da44b4b94e0341c5c9aff85
fujimurakengo <naruawaryousuke@gmail.com>
2021-08-15 20:48:36 +0900

    はじめてのコミット

---
+++ lib/bass_wrapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bass_wrapper/version"
+
+module BassWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bass_wrapper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BassWrapper
+  VERSION = "0.1.0"
+end
